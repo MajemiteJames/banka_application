07583aa096eb5c0834e789b1c84ed67f9ebf7b59
MajemiteJames <majemiteokoro@gmail.com>
2019-03-27 22:01:59 +0100

    chore(description) created my server file My port is currently working

---
+++ build/server.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express.default)();
+var port = process.env.PORT || 4040;
+app.get('/', function (req, res) {
+  res.send('Virtual Reality is the future');
+});
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
