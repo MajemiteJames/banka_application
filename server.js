c25a4d37a26ee11a4c50072f0273cc1b27373353
MajemiteJames <majemiteokoro@gmail.com>
2019-03-31 21:23:17 +0100

    chore(description) working on endpoint

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import accounts from './server/datastore/accounts';
 
 
 const app = express();
+
+// get all todos
+app.get('/api/v1/accounts', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'Accounts retrieved successfully',
+    accounts: accounts
+  })
+});
 
 const port = process.env.PORT || 4040;
 
---
+++ server/datastore/account.js
@@ -0,0 +1,43 @@
+/* eslint linebreak-style: "off" */
+
+/**
+ * a list of all red-flags and incidents
+ * @constant
+ *
+ * @type {Array<Object>}
+ * @exports accounts
+ */
+const accounts = [
+    {
+      id: 1,
+      createdOn: new Date().toISOString(),
+      accountNumber: 2578433446,
+      firstName: 'Ayomide',
+      lastName: 'Tosan',
+      email: 'ayomidetosan@gmail.com',
+      type: 'savings',
+      openingBalance: 15000.00,
+    },
+    {
+      id: 2,
+      createdOn: new Date().toISOString(),
+      accountNumber: 3872984638,
+      firstName: 'Majemite',
+      lastName: 'Okoro',
+      email: 'jamesokoro@gmail.com',
+      type: 'current',
+      openingBalance: 65000.00,
+    },
+    {
+      id: 3,
+      createdOn: new Date().toISOString(),
+      accountNumber: 193732839,
+      firstName: 'James',
+      lastName: 'Bachor',
+      email: 'jamesbachor@gmail.com',
+      type: 'savings',
+      openingBalance: 95000.00,
+    },
+  ];
+  
+  export default accounts;
