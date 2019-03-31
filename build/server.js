9dc0cce193f5db4b61513e170abe42341cce0f67
MajemiteJames <majemiteokoro@gmail.com>
2019-04-01 00:30:27 +0100

    chore(description) adjusted my build file

--- build/server.js
+++ build/server.js
@@ -2,19 +2,9 @@
 
 var _express = _interopRequireDefault(require("express"));
 
-var _savings = _interopRequireDefault(require("./server/routes/savings"));
-
-var _bodyParser = _interopRequireDefault(require("body-parser"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express.default)(); // Parse incoming requests data
-
-app.use(_bodyParser.default.json());
-app.use(_bodyParser.default.urlencoded({
-  extended: false
-}));
-app.use(_savings.default);
+var app = (0, _express.default)();
 var port = process.env.PORT || 4040;
 app.get('/', function (req, res) {
   res.send('The Port for my Banking App');
