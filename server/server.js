e07c51f070b9d437084ff86810b06f301b94c918
MajemiteJames <majemiteokoro@gmail.com>
2019-04-01 11:56:50 +0100

    chore(description): set up ci on develop branch

--- server/server.js
+++ server/server.js
@@ -14,9 +14,11 @@ app.use(router);
 const port = process.env.PORT || 4040;
 
 app.get('/', (req, res) => {
-  res.send('The Port for my Banking App');
+  res.status(200).send('The Port for my Banking App');
 });
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
 });
+
+export default app;
