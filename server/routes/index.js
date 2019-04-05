64cc554221d682a9ad66aa40b89ee5aa7a99ca9e
MajemiteJames <majemiteokoro@gmail.com>
2019-04-05 12:11:49 +0100

    bug(fix bug): bug fixed fix a bug in my test

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import accountController from '../Controllers/account';
-import transctionController from '../Controllers/debit';
+
 
 const router = express.Router();
 
@@ -9,11 +9,11 @@ router.get('/api/v1/accounts', accountController.getAllAccount);
 
   
 router.post('/api/v1/accounts', accountController.createAccount);
-router.post('/api/v1/transaction/account-number', transctionController.createTransaction);
+
 
 
 router.get('/api/v1/accounts/:id', accountController.getAccount);
-router.get('/api/v1/transaction/account-number/:id', transctionController.getSavings);
+
   
 router.delete('/api/v1/accounts/:id', accountController.deleteAccount);
   
