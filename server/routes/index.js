8f1616238026960e586d1204bce4aa9813a35176
MajemiteJames <majemiteokoro@gmail.com>
2019-04-10 14:14:21 +0100

    ch(User URL): Create user endpoint Created endpoint so a user can sign upinto the add and a token would be generated also use same token to login [finish: #164860975]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import accountController from '../Controllers/account';
+import Users from '../Controllers/users';
 
 
 const router = express.Router();
@@ -9,6 +10,9 @@ router.get('/api/v1/accounts', accountController.getAllAccount);
 
   
 router.post('/api/v1/accounts', accountController.createAccount);
+router.post('/api/v1/auth/signup', Users.signup);
+router.post('/api/v1/auth/signin', Users.login);
+
 
 
 
@@ -16,6 +20,7 @@ router.get('/api/v1/accounts/:id', accountController.getAccount);
 
   
 router.delete('/api/v1/accounts/:id', accountController.deleteAccount);
+//router.delete('/api/v1/accounts/:id', Users.deleteUser);
   
 
 router.patch('/api/v1/accounts/:id', accountController.updateAccount);
