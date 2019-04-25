1592eed3bfa4b9259c4b5ce5a067d141ffbe25e5
MajemiteJames <majemiteokoro@gmail.com>
2019-04-25 14:31:30 +0100

    ch(file recovery): Recover last files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import accountController from '../Controllers/Account';
-import AuthController from '../Controllers/Users';
+import User from '../dbController/userController';
+//import AuthController from '../Controllers/Users';
 import transactionController from '../Controllers/Transaction';
 import AuthValidation from '../middlewares/validateSignUp';
 import validateTransaction from '../middlewares/validateTransaction';
@@ -8,21 +9,21 @@ import verifyToken from '../middlewares/verifyToken'
 
 
 const router = express.Router();
-const { signUp, signIn } = AuthController;
+//const { signUp, signIn } = AuthController;
 const { validateUserSignup, validateUserLogIn } = AuthValidation;
 const { creditAccount, debitAccount } = transactionController;
 const { validateCreditTransaction, validateDebitTransaction } = validateTransaction;
 const { checkToken } = verifyToken;
 
-// get all todos
+
 router.get('/accounts', accountController.getAllAccount);
 
   
 router.post('/accounts',checkToken, accountController.createAccount);
 router.post('/transactions/:accountNumber/credit', validateCreditTransaction, creditAccount);
 router.post('/transactions/:accountNumber/debit' , validateDebitTransaction, debitAccount);
-router.post('/auth/signup',validateUserSignup, signUp);
-router.post('/auth/signin', validateUserLogIn , signIn);
+router.post('/auth/signup',validateUserSignup, User.create);
+router.post('/auth/signin', validateUserLogIn , User.login);
 
 
 
